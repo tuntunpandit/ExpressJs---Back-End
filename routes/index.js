ec60fb558e2bdd86235663e78a9ae4aa6b90e9bf
Tuntun Kumar <tuntun@hrgdev.com>
2021-05-10 20:13:01 +0530

    changing all api , doing some optimization in routing apis

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+import { registerController } from '../controllers';
+
+router.post('/register', registerController.register);
+
+export default router;
